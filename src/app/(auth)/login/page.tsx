6281aa0b455cb8f1afa5fec2c61d5625ce6b0a27
RafaelFink-dev <rafaelfinkanselmo99@gmail.com>
2025-07-30 20:55:51 -0300

    feat: Add images/logo watchlist

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,20 +1,9 @@
-"use client"
 
-import Modal from "@/components/ComponentModalTest";
-import Button from "@/components/ui/ButtonTest";
-import { useState } from "react";
 
 export default function LoginPage() {
-    const [isOpen, setIsOpen] = useState(false);
-
   return (
-    // This is a simple login page with buttons and a modal for testing purposes
     <div className="flex items-center justify-center flex-col gap-4 h-screen">
-      <h1 className="text-gray-400 text-4xl">Login Teste</h1>
-      <Button variant="primary">Clique aqui</Button>
-      <Button variant="secondary">Cancelar</Button>
-      <Button onClick={() => setIsOpen(true)}>Abrir Modal</Button>
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <h1 className="text-gray-400 text-4xl">LOGIN PAGE</h1>
     </div>
   );
 }
