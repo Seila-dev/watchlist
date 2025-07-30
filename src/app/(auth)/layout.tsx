de2d1818aa4d43f3c23f54690c9225bb9cc4aa75
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-07-30 14:09:14 -0300

    fix: fix all layout.tsx hydration errors

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,7 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <body>
-        <main className="">
-          {children}
-        </main>
-      </body>
-    </html>
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+      {children}
+    </main>
   );
 }
--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,9 +1,7 @@
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
       <body>
         <main className="">{children}</main>
       </body>
-    </html>
   );
 }
