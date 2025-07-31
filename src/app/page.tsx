a07e6c183c3029bbdf0b4ec90690844017d2051d
RafaelFink-dev <rafaelfinkanselmo99@gmail.com>
2025-07-30 22:06:52 -0300

    feat: Add tailwind config and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center flex-col gap-4 h-screen w-full max-w-5xl mx-auto">
+    <div className=" flex items-center justify-center flex-col gap-4 h-screen w-full max-w-5xl mx-auto">
 
       <h1 className="text-black text-5xl font-bold">ÁREA DE TESTE DE COMPONENTES</h1>
 
       <Image
-        alt="teste"
+        alt="testeLogo"
         src={'/assets/logos/watchlist-logo-dark.webp'}
         width={150}
         height={80}
       />
       <h1 className="text-black text-5xl font-bold">HOME PAGE</h1>
 
-      <Button>
-        TESTE BOTÃO
+      <Button className="font-bold w-48 p-3 text-white">
+        ENTRAR
       </Button>
+
+      <span className="font-bold">Input abaixo com focus indigo-700:</span>
       <Input />
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",  
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#EEF2FF',
+          100: '#E0E7FF',
+          200: '#C7D2FE',
+          300: '#A5B4FC',
+          400: '#818CF8',
+          500: '#6366F1',
+          600: '#4F46E5',
+          700: '#4338CA',
+          800: '#3730A3',
+          900: '#312E81',
+        },
+        secondary: {
+          50: '#F5F3FF',
+          100: '#EDE9FE',
+          200: '#C4B5FD',
+          300: '#C4B5FD',
+          400: '#A78BFA',
+          500: '#8B5CF6',
+          600: '#7C3AED',
+          700: '#6D28D9',
+          800: '#5B21B6',
+          900: '#4C1D95',
+        },
+        grayBrand: {
+          50: '#F9FAFB',
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
+        },
+        gray: {
+          50: '#FAFAFA',
+          100: '#F4F4F5',
+          200: '#E4E4E7',
+          300: '#D4D4D8',
+          400: '#A1A1AA',
+          500: '#71717A',
+          600: '#52525B',
+          700: '#3F3F46',
+          800: '#27272A',
+          900: '#18181B',
+        },
+        neutral: {
+          50: '#ffffff',  // evitei o 0, porque pode dar problema
+          900: '#000000',
+        },
+        systemStatus: {
+          error: '#EF4444',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
