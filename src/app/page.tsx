294e9b65cdb215026323e9d5740abc3a1b65db7d
RafaelFink-dev <rafaelfinkanselmo99@gmail.com>
2025-07-30 21:12:22 -0300

    feat: add inter fonts for typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
-import { SmileyNervous } from "phosphor-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col gap-4 h-screen">
-      <h1 className="text-black text-4xl">HOME PAGE</h1>
-      <SmileyNervous width={50} height={50}/>
+      <Image
+        alt="teste"
+        src={'/assets/logos/watchlist-logo-dark.webp'}
+        width={150}
+        height={80}
+      />
+      <h1 className="text-black text-5xl font-bold">HOME PAGE</h1>
     </div>
   );
 }
