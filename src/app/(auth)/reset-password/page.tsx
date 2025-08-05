285149eb13737255d3712fb3262299dbb3e91b2a
RafaelFink-dev <rafaelfinkanselmo99@gmail.com>
2025-08-04 22:08:21 -0300

    feat: Middleware.ts

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -4,7 +4,6 @@ export default function ResetPassword() {
   return (
     <div className="flex items-center justify-center flex-col gap-4 h-screen">
       <h1 className="text-gray-400 text-4xl">RESET-PASSWORD PAGE</h1>
-      <VerifyEmailModal email="rafaelfinkanselmo99@gmail.com"/>
     </div>
   );
 }
