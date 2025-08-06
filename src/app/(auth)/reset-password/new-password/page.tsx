cb92f29fc06560db5a26ba42221b7f1963b15f14
RafaelFink-dev <rafaelfinkanselmo99@gmail.com>
2025-08-05 22:25:14 -0300

    fix: adjust code sending and pass email between register and reset password pages

---
+++ src/app/(auth)/reset-password/new-password/page.tsx
@@ -0,0 +1,9 @@
+import { VerifyEmailModal } from "@/components/VerifyEmailCode/VerifyEmailModal";
+
+export default function ResetPassword() {
+  return (
+    <div className="flex items-center justify-center flex-col gap-4 h-screen">
+      <h1 className="text-gray-400 text-4xl">NEW-PASSWORD PAGE</h1>
+    </div>
+  );
+}
