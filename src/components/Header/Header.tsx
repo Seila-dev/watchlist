45b1eec306663fed96b778efebf197158f2c76e2
Mauricio Chiapetta <m.faisca1234@gmail.com>
2025-08-19 16:08:36 -0300

    feat: implement Header Component with logo, nav links, add button, notifications icon, user icon

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Image from "next/image";
+import Logo from "../../../public/assets/logos/watchlist-logo-white.webp";
+import Link from "next/link";
+import { ArrowUpRight, Plus } from "lucide-react";
+import { User, Bell } from "phosphor-react";
+import { Button } from "../ui/button";
+
+const links = [
+  {
+    label: "Ínicio",
+    href: "/",
+  },
+  {
+    label: "Álbum de conteúdos",
+    href: "/contents",
+  },
+  {
+    label: "Favoritos",
+    href: "/favorites",
+  },
+  {
+    label: "Comunidade",
+    href: "/community",
+    icon: ArrowUpRight,
+  },
+];
+
+export function Header() {
+  return (
+    // ta quebrando o layout !!!
+    <header className="bg-[#18181B] flex items-center justify-between py-4 px-10 border-b border-gray-800">
+      <Link href="/">
+        <Image src={Logo} alt="Watchlist Logo" width={125} height={24} />
+      </Link>
+
+      <nav className="flex items-center gap-12 text-gray-400">
+        {links.map(({ label, href, icon: Icon }) => (
+          <Link
+            key={href}
+            href={href}
+            className="flex items-center hover:text-white duration-300 transition-all gap-2"
+          >
+            {label}
+            {Icon && <Icon size={20} />}
+          </Link>
+        ))}
+      </nav>
+
+      <div className="flex items-center gap-4 flex-shrink-0">
+        {/* adicionar funcionalidade onClick() no Button */}
+        <Button className="text-white w-[121px]" variant={"default"}>
+          <Plus />
+          Adicionar
+        </Button>
+        <span className="flex items-center gap-3">
+          {/* notificações */}
+          <Bell
+            size={25}
+            className="text-gray-400 hover:text-white duration-300 cursor-pointer"
+          />
+          {/* perfil do usuário */}
+          <User
+            size={25}
+            className="text-gray-400 hover:text-white duration-300 cursor-pointer"
+          />
+        </span>
+      </div>
+    </header>
+  );
+}
