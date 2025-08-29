bd502417716f1be1634cefcf1adf9f7c237d11d0
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-08-29 18:03:55 -0300

    fix: add condition to middleware when user is logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,10 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-
   const token = request.cookies.get('watchlist.token');
-
   const url = request.nextUrl.clone();
 
+  // Se não tem token e tenta acessar páginas protegidas
   if (!token && url.pathname.startsWith('/home')) {
     url.pathname = '/login';
     return NextResponse.redirect(url);
@@ -19,9 +18,14 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  if (token && (url.pathname === '/login' || url.pathname.startsWith('/register'))) {
+    url.pathname = '/home';
+    return NextResponse.redirect(url);
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/home/:path*', '/register/create-username', '/home'],
+  matcher: ['/home/:path*', '/register/:path*', '/login', '/home'],
 };
